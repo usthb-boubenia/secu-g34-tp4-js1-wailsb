9b1f010dbe19673184a2cc6b35797518a64775b7
wail.saribey <wail.saribey@gmail.com>
2025-11-03 21:06:45 +0100

    exo 1

---
+++ exo1/script.js
@@ -0,0 +1,27 @@
+"use strict"
+// function f1 pour qfficher nom
+function F1() {
+    console.log("John");
+}
+// function f2 pour prenom
+function F2() {
+    console.log("Doe");
+}
+// function f3 pour nom prenom
+
+function F3() {
+    console.log("Jon-Doe");
+}
+// function f4 pour age
+
+function F4() {
+    console.log("30");
+}
+// function f5 pour note
+function F5() {
+    console.log("15");
+}
+// function f6 pour pi
+function F6() {
+    console.log("3.14");
+}
