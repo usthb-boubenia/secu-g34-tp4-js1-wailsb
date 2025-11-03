536c3ff1e49bafbc6b40e116c20e2064b6afb4f7
wail.saribey <wail.saribey@gmail.com>
2025-11-03 21:49:52 +0100

    fixed errors done

--- exo3/script.js
+++ exo3/script.js
@@ -1,13 +1,13 @@
 "use strict"
 // definition des etudiants
 let Etudiants=[
-    {id: 1000, nom: "JOHN", prenom: "DOE", moy: 14, section: 5},
-    {id: 2000, nom: "BOB", prenom: "CARLTON", moy: 7, section: 1},
-    {id: 3000, nom: "RAYANE", prenom: "SMITH", moy: 13, section: 3}
+    {id: 1000, nom: "JOHN", prenom: "DOE", moy: 14, moy2: 5},
+    {id: 2000, nom: "BOB", prenom: "CARLTON", moy: 7, moy2: 1},
+    {id: 3000, nom: "RAYANE", prenom: "SMITH", moy: 13, moy2: 3}
 ]
 // function B
 function B(moy){
-    return moy>10.0;
+    return ((moy+moy2)/2)+5>10.0;
 }
 // function A pour deliberation
 function A(){
