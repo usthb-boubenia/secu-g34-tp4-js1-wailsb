86a13378f08c3a30a3ad880cbe488396fd2b4a53
wail sari bey <wail.saribey@gmail.com>
2025-11-08 10:05:13 +0100

    added comments of slides

--- exo1/script.js
+++ exo1/script.js
@@ -1,27 +1,33 @@
 "use strict"
-// function f1 pour qfficher nom
+// function f1 pour qfficher nom page 46
 function F1() {
+    // console log page 33
     console.log("John");
 }
-// function f2 pour prenom
+// function f2 pour prenom page 46
 function F2() {
+    // console log page 33
     console.log("Doe");
 }
-// function f3 pour nom prenom
+// function f3 pour nom prenom page 46
 
 function F3() {
+    // console log page 33
     console.log("Jon-Doe");
 }
-// function f4 pour age
+// function f4 pour age page 46
 
 function F4() {
+    // console log page 33
     console.log("30");
 }
-// function f5 pour note
+// function f5 pour note page 46
 function F5() {
+    // console log page 33
     console.log("15");
 }
-// function f6 pour pi
+// function f6 pour pi page 46
 function F6() {
+    // console log page 33
     console.log("3.14");
 }
