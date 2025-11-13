d70feb77da113ca212ad5d21f2ec386d7cd2603a
wail sari bey <wail.saribey@gmail.com>
2025-11-13 08:58:32 +0100

    added let vars finale fixes

--- exo1/script.js
+++ exo1/script.js
@@ -2,29 +2,34 @@
 // function f1 pour qfficher nom page 46
 function F1() {
     // console log page 33
-    console.log("John");
+    let name="John"
+    console.log(name);
 }
 // function f2 pour prenom page 46
 function F2() {
     // console log page 33
-    console.log("Doe");
+    let lname="Doe"
+    console.log(lname);
 }
 // function f3 pour nom prenom page 46
 
 function F3() {
     // console log page 33
-    console.log("Jon-Doe");
+    let fullname="Jon-Doe"
+    console.log(fullname);
 }
 // function f4 pour age page 46
 
 function F4() {
     // console log page 33
-    console.log("30");
+    let age=30
+    console.log(age);
 }
 // function f5 pour note page 46
 function F5() {
     // console log page 33
-    console.log("15");
+    let mark=15
+    console.log(mark);
 }
 // function f6 pour pi page 46
 function F6() {
